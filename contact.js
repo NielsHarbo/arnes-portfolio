236d0f97758dd261f738ef202dbbdbedfabd0de2
NielsHarbo <nha@rts.dk>
2023-01-04 11:31:15 +0100

    Classes added

--- contact.js
+++ contact.js
@@ -1,22 +1,25 @@
+//url fra adresselinjen
 let urlString = window.location
-// console.log(urlString)
+//Vi konverterer url fra tekststeng til et objekt
 let urlObject = new URL(urlString)
+//Den div på siden hvor svar-beskeden skal stå
 let messageDiv = document.getElementById("message")
-// console.log(messageDiv)
-let html
 
+//HTML indhold til svar-beskeden
+//bemærk [+] asigner en værdi [+=] tilføjer mere til værdien
+let html
 html = "<h1>"
-html += "Hej "
-html += urlObject.searchParams.get("your-name")
+    html += "Hej "
+    html += urlObject.searchParams.get("your-name")
 html += "</h1>"
 
 html += "<p>"
-html += "Tak for din besked"
+    html += "Tak for din besked"
 html += "</p>"
 
 html +="<p>"
-html += "Vi sender dig et svar til: "
-html += urlObject.searchParams.get("your-email")
+    html += "Vi sender dig et svar til: "
+    html += urlObject.searchParams.get("your-email")
 html += "</p>"
 
 messageDiv.innerHTML = html
